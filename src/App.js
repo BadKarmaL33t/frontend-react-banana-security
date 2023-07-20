cf7a4a24eb4a37d52ebb8a39889d878ccd3f68f5
BadKarmaL33t <jchagesteijn@protonmail.com>
2023-07-20 17:55:55 +0200

    profielpagina voorzien van een private route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Profile from './pages/Profile';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import {AuthContext} from "./context/AuthContext";
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <NavBar />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
-      </div>
-    </>
-  );
+    const {isAuth} = useContext(AuthContext);
+
+    return (
+        <>
+            <NavBar/>
+            <div className="content">
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route
+                        path="/profile"
+                        element={isAuth ? <Profile/> : <Home/>}
+                        />
+
+                    <Route path="/signin" element={<SignIn/>}/>
+                    <Route path="/signup" element={<SignUp/>}/>
+                </Routes>
+            </div>
+        </>
+    );
 }
 
 export default App;
